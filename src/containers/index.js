e5d3fd36b2c9c169e90364239d45afc08c9d0ede
lette1394 <lette1394+github@gmail.com>
2017-11-23 13:22:19 +0900

    [ADD] write.js modify.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,6 +3,8 @@ import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import Wall from './Wall';
+import Modify from './Modify';
+import WriteContainer from './WriteContainer';
 
 
-export { App, Home, Login, Register, Wall };
+export { App, Home, Login, Register, Wall, Modify, WriteContainer };
